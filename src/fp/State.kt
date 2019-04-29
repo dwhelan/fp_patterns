b8b37706eeab4addead21841f09986d6661d53d7
Declan Whelan <dwhelan@dpwhelan.com>
2019-04-29 18:12:18 -0500

    Make Uppercase have a single method

--- src/fp/State.kt
+++ src/fp/State.kt
@@ -2,30 +2,28 @@ package fp
 
 data class Result(val string: String, val state: State) {}
 
-data class State(val processor: Processor, val data: Int = 0) {}
+data class State(val name: EchoState, val data: Int = 0) {}
 
 fun echo(string: String, state: State = State(LowerCase())) =
-    state.processor.echo(string, state)
+    state.name.echo(string, state)
 
-interface Processor {
+interface EchoState {
     fun echo(string: String, state: State): Result
 }
 
-class LowerCase : Processor {
+class LowerCase : EchoState {
     override fun echo(string: String, state: State) =
         Result(string.toLowerCase(), State(UpperCase()))
 }
 
-class UpperCase : Processor {
+class UpperCase : EchoState {
     override fun echo(string: String, state: State) =
-        Result(string.toUpperCase(), transitions(state))
-
-    private fun transitions(state: State): State {
-        if (state.data > 0) {
-            return State(LowerCase())
-        } else {
-            return State(UpperCase(), state.data + 1)
-        }
-    }
-
+        Result(
+            string.toUpperCase(),
+            if (state.data > 0) {
+                State(LowerCase())
+            } else {
+                State(UpperCase(), state.data + 1)
+            }
+        )
 }
--- src/fp/State2.kt
+++ src/fp/State2.kt
@@ -2,21 +2,21 @@ package fp2
 
 data class Result(val string: String, val state: State) {}
 
-data class State(val name: StateName, val data: Int = 0)
+data class State(val name: EchoState, val data: Int = 0)
 
 fun echo(string: String, state: State = State(LOWER_CASE)) =
     Result(act(string, state), next(state))
 
-enum class StateName { LOWER_CASE, UPPER_CASE }
-val LOWER_CASE = StateName.LOWER_CASE
-val UPPER_CASE = StateName.UPPER_CASE
+enum class EchoState { LOWER_CASE, UPPER_CASE }
+val LOWER_CASE = EchoState.LOWER_CASE
+val UPPER_CASE = EchoState.UPPER_CASE
 
-val actions = mapOf<StateName, (String) -> String> (
+val actions = mapOf<EchoState, (String) -> String> (
     LOWER_CASE to { string -> string.toLowerCase() },
     UPPER_CASE to { string -> string.toUpperCase() }
 )
 
-val transitions = mapOf<StateName, (State) -> State> (
+val transitions = mapOf<EchoState, (State) -> State> (
     LOWER_CASE to { _ -> State(UPPER_CASE) },
     UPPER_CASE to { state ->
         if (state.data > 0)
