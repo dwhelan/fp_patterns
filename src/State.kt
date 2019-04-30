f17cfbb484c31750ff873bebbe10087d6bb890b6
Declan Whelan <dwhelan@dpwhelan.com>
2019-04-30 17:05:19 -0500

    Extract upperCase()

--- src/State.kt
+++ src/State.kt
@@ -1,23 +1,19 @@
-
 typealias Echoer = (String, State) -> Result
+
+data class State(val echoer: Echoer, val count: Int = 0) {}
+
+data class Result(val string: String, val state: State) {}
 
 fun echo(string: String, state: State = State(lowerCase)) =
     state.echoer(string, state)
 
-data class Result(val string: String, val state: State) {}
-
-data class State(var echoer: Echoer, val count: Int = 0) {}
-
 val lowerCase: Echoer = { string, state ->
-    state.echoer = upperCase
-    Result(string.toLowerCase(), state)
+    Result(string.toLowerCase(), State(upperCase))
 }
 
 val upperCase: Echoer = { string, state ->
-    var count = state.count + 1
-    fun isSecondTime() = 1 < count
-    if (isSecondTime())
-        state.echoer = lowerCase
-    Result(string.toUpperCase(), State(state.echoer, count))
+    if (0 < state.count)
+        Result(string.toUpperCase(), State(lowerCase))
+    else
+        Result(string.toUpperCase(), State(state.echoer, state.count + 1))
 }
-
